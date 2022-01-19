9eca706d35e08d23fd627f3ab9432ea5d3feb8fc
Alyxia Sother <lexisoth2005@gmail.com>
2022-01-19 20:54:29 +0100

    [version] Add version display

---
+++ src/version.js
@@ -0,0 +1,30 @@
+sc.TitleScreenGui.inject({
+  crosscodePLItem: null,
+  init(...args) {
+    this.parent(...args);
+
+    let version = modloader.loadedMods.get('CrossCode-PL').version;
+    let versionGui = new sc.TextGui(`PL v${version}`, {
+      font: sc.fontsystem.tinyFont,
+    });
+    let displayPos = this.ccloaderVersionGui.hook.size.y;
+    versionGui.setAlign(this.ccloaderVersionGui.hook.align.x, this.ccloaderVersionGui.hook.align.y);
+
+    // TODO: Make a PR to `ccloader-version-display` that adds an API for
+    // standardising addition of version display items Hardcoding a list like
+    // this is stupid and should be avoided at all costs, but well, it's not
+    // like I have another option right now
+    const verMods = [
+      modloader.loadedMods.get('crosscode-ru'),
+      modloader.loadedMods.get('CrossCode-Esp'),
+      modloader.loadedMods.get('autumns-genesis'),
+    ];
+    for (let mod of verMods) {
+      if (mod === undefined) continue;
+      displayPos += 8;
+    }
+
+    versionGui.setPos(0, displayPos);
+    this.ccloaderVersionGui.addChildGui(versionGui);
+  },
+});
