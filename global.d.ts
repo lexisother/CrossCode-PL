1c55e0bf5061cf884091631248499c3efee8b857
Alyxia Sother <lexisoth2005@gmail.com>
2022-01-24 21:20:39 +0100

    [types] Import our headers

--- global.d.ts
+++ global.d.ts
@@ -1,2 +1,5 @@
 import 'ultimate-crosscode-typedefs/modloader';
 import 'ultimate-crosscode-typedefs/crosscode';
+import './src/headers/version';
+import './src/headers/Localize-Me';
+import './src/headers/title-screen';
