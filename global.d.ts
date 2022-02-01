5c6c0428c84bd9cbd549478d4ea3a464df77ba51
Alyxia Sother <lexisoth2005@gmail.com>
2022-02-01 13:10:34 +0100

    [Meta] Use Localize-Me typedefs from UCCTD

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,6 @@
 import 'ultimate-crosscode-typedefs/modloader';
 import 'ultimate-crosscode-typedefs/crosscode';
+import 'ultimate-crosscode-typedefs/mods/Localize-me';
 import './src/headers/version';
 import './src/headers/Localize-Me';
 import './src/headers/title-screen';
