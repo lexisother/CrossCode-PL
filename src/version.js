124a1d32b167817ea2f24ce4745ea17f471e1f80
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2022-05-02 02:07:49 +0300

    use the standard version label snippet (#2)

--- src/version.js
+++ src/version.js
@@ -8,37 +8,31 @@ ig.module('cc-pl.version')
     'ccloader-runtime.ui.version-display',
   )
   .defines(() => {
+    const VERSION_TEXT_STR = `PL v${modloader.loadedMods.get('CrossCode-PL').version}`;
+
+    function attachVersionTextGui(self) {
+      if (self.modVersionGuis == null) {
+        self.modVersionGuis = [self.ccloaderVersionGui];
+      }
+      let prevGui = self.modVersionGuis.last();
+      let newGui = new sc.TextGui(VERSION_TEXT_STR, { font: sc.fontsystem.tinyFont });
+      newGui.setAlign(prevGui.hook.align.x, prevGui.hook.align.y);
+      newGui.setPos(prevGui.hook.pos.x, prevGui.hook.pos.y + prevGui.hook.size.y);
+      self.versionGui.addChildGui(newGui);
+      self.modVersionGuis.push(newGui);
+    }
+
     sc.TitleScreenGui.inject({
-      crosscodePLItem: null,
-      init() {
-        this.parent();
+      init(...args) {
+        this.parent(...args);
+        attachVersionTextGui(this);
+      },
+    });
 
-        let version = modloader.loadedMods.get('CrossCode-PL').version.toString();
-        let versionGui = new sc.TextGui(`PL v${version}`, {
-          font: sc.fontsystem.tinyFont,
-        });
-        let displayPos = this.ccloaderVersionGui.hook.size.y;
-        versionGui.setAlign(
-          this.ccloaderVersionGui.hook.align.x,
-          this.ccloaderVersionGui.hook.align.y,
-        );
-
-        // TODO: Make a PR to `ccloader-version-display` that adds an API for
-        // standardising addition of version display items Hardcoding a list like
-        // this is stupid and should be avoided at all costs, but well, it's not
-        // like I have another option right now
-        const verMods = [
-          modloader.loadedMods.get('crosscode-ru'),
-          modloader.loadedMods.get('CrossCode-Esp'),
-          modloader.loadedMods.get('autumns-genesis'),
-        ];
-        // for (let mod of verMods) {
-        //   if (mod === undefined) continue;
-        //   displayPos += 8;
-        // }
-
-        versionGui.setPos(0, displayPos);
-        this.ccloaderVersionGui.addChildGui(versionGui);
+    sc.PauseScreenGui.inject({
+      init(...args) {
+        this.parent(...args);
+        attachVersionTextGui(this);
       },
     });
   });
